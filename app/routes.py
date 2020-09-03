454208771256009e5f675d11276c55aaa8cfa6fd
Benjamin <b.wiessneth@googlemail.com>
2020-09-03 10:59:00 +0200

    Added DELETE for User and Department ressource

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app, db
 from . import models, schemas
 from typing import List
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Response, status
 
 
+@app.get("/users", response_model=schemas.UserListOut)
 @app.get("/users/", response_model=schemas.UserListOut)
 def read_users(request:Request, offset=0, limit=100):
 	total = db.query(models.User).count()
@@ -30,6 +31,18 @@ def read_users(request:Request, user_id):
 	return {'data':user}
 
 
+@app.delete("/users/{user_id}")
+def delete_user(user_id):
+	user = db.query(models.User).get(user_id)
+	if user:
+		db.delete(user)
+		db.commit()
+		return Response(status_code=status.HTTP_204_NO_CONTENT)
+	else:
+		raise HTTPException(status_code=404, detail=f"Could not find User with ID {user_id}")
+
+
+@app.get("/departments", response_model=schemas.DepartmentListOut)
 @app.get("/departments/", response_model=schemas.DepartmentListOut)
 def read_departments(request:Request, offset=0, limit=100):
 	total = db.query(models.Department).count()
@@ -38,6 +51,7 @@ def read_departments(request:Request, offset=0, limit=100):
 		_links = {}
 		_links.update({"self" : request.url_for("read_departments", department_id=department.id)})
 		_links.update({"collection" : request.url_for("read_departments")})
+		_links.update({"users" : request.url_for("read_department_users", department_id=department.id)})
 		department.__setattr__("_links", _links)
 	return {'data':departments, 'total':total, 'offset':offset, 'limit':limit}
 
@@ -69,3 +83,14 @@ def read_department_users(request:Request, department_id:int, offset=0, limit=10
 		_links.update({"department" : request.url_for("read_department_users", department_id=user.department_id)})
 		user.__setattr__("_links", _links)
 	return {'data':users, 'total':total, 'offset':offset, 'limit':limit}
+
+
+@app.delete("/departments/{department_id}")
+def delete_department(department_id):
+	department = db.query(models.Department).get(department_id)
+	if department:
+		db.delete(department)
+		db.commit()
+		return Response(status_code=status.HTTP_204_NO_CONTENT)
+	else:
+		raise HTTPException(status_code=404, detail=f"Could not find Department with ID {department_id}")
